7d873e2e2c3f20f051a27a8f191202e7a4726aa1
Percy Tuncar <percy.edgar.tuncar@gmail.com>
2022-06-27 12:28:16 -0500

    Function system

---
+++ Function ceil.cpp
@@ -0,0 +1,16 @@
+// La función ceil() redondea un número hacia arriba.
+// Crear una aplicación que redondea un número hacia arriba.
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    float num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    cout << "El numero redondeado es: " << ceil(num) << endl;
+    return 0;
+}
+
+// Referencia:
+// Geeks for Geeks, 2022. Ceil and Floor functions in C++. En: Geeks for Geeks [en línea].
+// Disponible en: https://www.geeksforgeeks.org/ceil-floor-functions-cpp/  [consulta: 24 Junio 2022].
