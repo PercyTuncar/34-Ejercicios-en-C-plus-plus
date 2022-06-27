21df4f88ad7d99a3759700a879b18b5a23235833
Percy Tuncar <percy.edgar.tuncar@gmail.com>
2022-06-26 22:58:57 -0500

    funcion abs

---
+++ Funcion abs.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int num;
+    cout << "Ingrese un numero: ";
+    cin >> num;
+    cout << "El numero absoluto de " << num << " es: " << abs(num) << endl;
+}
